2853c543c51aa1dc7da330af8153ca6502e4cb2f
Seojin Oh <seojinoh.me@gmail.com>
2022-02-20 21:17:01 +0900

    feat : Add SecurityConfig for Spring Security Configuration

---
+++ src/main/java/me/seojinoh/learning/springSecurity/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package me.seojinoh.learning.springSecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import me.seojinoh.learning.springSecurity.service.MemberService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private MemberService memberService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+		webSecurity.ignoring().antMatchers("/lib/**", "/js/**", "/css/**", "/img/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests()
+			.antMatchers("/user/**").authenticated()
+			.antMatchers("/admin/**").authenticated()
+			.antMatchers("/**").permitAll();
+
+		httpSecurity.formLogin()
+			.loginPage("/login")
+			.defaultSuccessUrl("/")
+			.usernameParameter("email")
+			.passwordParameter("password")
+			.permitAll();
+
+		httpSecurity.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/login")
+			.invalidateHttpSession(true);
+
+		httpSecurity.exceptionHandling()
+			.accessDeniedPage("/denied");
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+	}
+
+}
